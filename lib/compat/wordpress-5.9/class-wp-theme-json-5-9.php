0d7f0ed2ddb9582549d6e50f6d041da2426ae53d
Glen Davies <glen.davies@a8c.com>
2022-04-14 17:03:58 +1200

    Declare the standard properties as well as vars to avoid having to declare them at block level

--- lib/compat/wordpress-5.9/class-wp-theme-json-5-9.php
+++ lib/compat/wordpress-5.9/class-wp-theme-json-5-9.php
@@ -946,6 +946,10 @@ class WP_Theme_JSON_5_9 {
 								'name'  => '--wp--user--preset--' . $property,
 								'value' => 'var(' . $css_var . ')',
 							),
+							array(
+								'name'  => $property,
+								'value' => 'var(' . $css_var . ')',
+							),
 						)
 					);
 				}
